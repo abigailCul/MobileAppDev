187c6c256dfcd3288bd312ca69a6b1892500568c
Abigail Cul <g00334291@gmit.ie>
2018-12-06 15:54:20 +0000

    volume slider

--- SpaceShooter/Assets/Scripts/VolumeChanger.cs
+++ SpaceShooter/Assets/Scripts/VolumeChanger.cs
@@ -4,6 +4,26 @@ using UnityEngine;
 
 public class VolumeChanger : MonoBehaviour {
 
+    //audio component
+    private AudioSource audioSrc;
+
+    //Music slider
+    private float musicVolume = 1f;
+
+    void Start ()
+    {
+        audioSrc = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        audioSrc.volume = musicVolume;
+    }
+
+    public void setVolume(float vol)
+    {
+        musicVolume = vol;
+    }
 
 public void ToggleSound()
     {
